b5bbad25a5a3b4b71e19eef96cbff4063cd9faad
jehyun Park <adam991217@naver.com>
2021-01-28 11:37:17 +0900

    SetUp

---
+++ src/test/java/com/iso/logus/LogusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iso.logus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
