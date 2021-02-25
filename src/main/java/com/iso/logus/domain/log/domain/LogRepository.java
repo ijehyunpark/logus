88be60a181ae20a7e61f23e49c2de45fd6684aa9
jehyun Park <adam991217@naver.com>
2021-02-25 14:04:32 +0900

    Fix add param annotation

--- src/main/java/com/iso/logus/domain/log/domain/LogRepository.java
+++ src/main/java/com/iso/logus/domain/log/domain/LogRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LogRepository extends JpaRepository<Log, Long>{
 	Page<Log> findAllByTeamId(long teamId, Pageable pageable);
@@ -12,6 +13,6 @@ public interface LogRepository extends JpaRepository<Log, Long>{
 	Page<Log> findAllByTeamIdAndContentContaining(long teamId, String content, Pageable pageable);
 	Page<Log> findAllByTeamIdAndAuthorContaining(long teamId, String uid, Pageable pageable);
 	
-	@Query(value = "SELECT l FROM Log l WHERE l.team.id = :team_id AND (l.title = :keyword OR l.content = :keyword)")	
-	Page<Log> findAllByTeamIdAndKeywordContaining(long teamId, String keyword, Pageable pageable);
+	@Query(value = "SELECT l FROM Log l WHERE l.team.id = :teamId AND (l.title = :keyword OR l.content = :keyword)")	
+	Page<Log> findAllByTeamIdAndKeywordContaining(@Param("teamId") long teamId, @Param("keyword") String keyword, Pageable pageable);
 }
